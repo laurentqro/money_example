10abcc3c6a49cbc8bfb36e2e5c5ad166c0a0e63f
Laurent Curau <laurentcurau@gmail.com>
2018-10-17 10:19:45 +0100

    Remove now redundant tests

--- spec/money_example_spec.rb
+++ spec/money_example_spec.rb
@@ -6,23 +6,15 @@ RSpec.describe MoneyExample do
     expect(MoneyExample::Money.dollar(15).equals?(five.times(3))).to be true
   end
 
-  it "compares dollars" do
+  it "compares money" do
     expect(MoneyExample::Money.dollar(5).equals?(MoneyExample::Money.dollar(5))).to be true
     expect(MoneyExample::Money.dollar(6).equals?(MoneyExample::Money.dollar(5))).to be false
+    expect(MoneyExample::Money.swiss_franc(6).equals?(MoneyExample::Money.dollar(6))).to be false
   end
 
   it "multiplies Swiss Francs" do
     five = MoneyExample::Money.swiss_franc(5)
     expect(MoneyExample::Money.swiss_franc(10).equals?(five.times(2))).to be true
-  end
-
-  it "compares Swiss Francs" do
-    expect(MoneyExample::Money.swiss_franc(5).equals?(MoneyExample::Money.swiss_franc(5))).to be true
-    expect(MoneyExample::Money.swiss_franc(6).equals?(MoneyExample::Money.swiss_franc(5))).to be false
-  end
-
-  it "compares across currencies" do
-    expect(MoneyExample::Money.swiss_franc(6).equals?(MoneyExample::Money.dollar(6))).to be false
   end
 
   it "has a currency" do
